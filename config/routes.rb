aa82abf0617883b32d3884df697e008608c49eaa
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2023-04-12 23:14:16 +0800

    设置article默认页

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'articles/index'
+  # 设置article的index设置为默认 并将别名设置为article
+  get 'articles', to: 'article#index', as: 'article' 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
