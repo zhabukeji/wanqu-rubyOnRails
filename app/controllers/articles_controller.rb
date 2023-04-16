9b0d5ff9b2e6cdd713082db65ca27a434caaa868
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2023-04-16 23:40:10 +0800

    添加articles action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def show
+    @article = Article.findby(params[:id])
+  end
+  def update
+    @article = Article.findby(params[:id])
+  end
+  def delete
+    @article = Article.findby(params[:id])
+  end 
 end
