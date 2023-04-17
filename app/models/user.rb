08d4d19b49d6451b4807443678e71436e84e63cc
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2023-04-17 22:08:17 +0800

    测试添加user validates方法

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates username,presence :{message: "用户名不能为空"}
+    validates username,uniqueness :{message: "用户名不能为空"}
+    validates username,presence :{message: "密码不能为空"}
+    validates username,length :{ minium:6, message: "密码最少为6位"}
 end
