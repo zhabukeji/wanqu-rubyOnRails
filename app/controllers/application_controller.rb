f27a812254f12b73d5d5271ea6fdfc8d677243b8
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2023-04-23 00:00:58 +0800

    修复bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     # 设置csrf验证不正确处理方法
+    include Concerns::LayoutsHelper
     protect_from_forgery with: :exception
     # 将方法暴露给views
-    include Concerns::LayoutsHelper
 end
--- app/controllers/concerns/layouts_helper.rb
+++ app/controllers/concerns/layouts_helper.rb
@@ -1,10 +1,10 @@
 module Concerns
     module LayoutsHelper
-        # def self.included Base
-        #     base.class_eval do
-        #         helper_method :getArticles, :getTopbars, :getWisdoms
-        #     end
-        # end
+        def self.included base
+            base.class_eval do
+                helper_method :getArticles, :getTopbars, :getWisdoms
+            end
+        end
         def getArticles
             @articles = Article.all
         end
@@ -14,4 +14,5 @@ module Concerns
         def getWisdoms
             @wisdoms = Wisdom.all
         end
+    end
 end
