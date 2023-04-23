8a98fa2d06368a328e0d591e81181e15e006b483
zhabukeji@foxmail.com <renzhabu@foxmail.com>
2023-04-23 23:38:50 +0800

    添加部分注释

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    # 设置csrf验证不正确处理方法
+    # 引入concerns包，里面封装部分controller需要的方法
     include Concerns::LayoutsHelper
+    # 设置csrf验证
     protect_from_forgery with: :exception
-    # 将方法暴露给views
 end
